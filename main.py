c8c24be079c4bfa54a9e9eede14c3cc94ca87458
Tunikov Dmitrii <dima.tunikov@mail.ru>
2020-03-10 13:43:09 +0300

    add removing old tracks

--- main.py
+++ main.py
@@ -10,12 +10,11 @@ class points_pair(object):
         self.xl = xl
         self.xr = xr
         self.center = (xr + xl) / 2
+        self.alive = False
 
 
 def cross_detect(edge_image):
     tic = time.perf_counter()
-
-    dimensions = edge_image.shape
 
     height = edge_image.shape[0]
     width = edge_image.shape[1]
@@ -23,10 +22,18 @@ def cross_detect(edge_image):
     max_dist_between_rails = 100
 
     pairs = [0] * height
+    tracks = [0] * height
+
     depth = 3
-    x_eps = 10
+    x_eps = 3
+    min_track_len = 50
     for y in reversed(range(height)):
         pairs[y] = []
+        tracks[y] = []
+
+        for yy in range(y + 1, min(y + depth + 1, height)):
+            for pair in pairs[yy]:
+                pair.alive = False
 
         #make correct min/max dist between rails function = f(y)
 
@@ -40,7 +47,29 @@ def cross_detect(edge_image):
                     continue
 
                 cur_pair = points_pair(x_l, x_r)
+                cur_track = [cur_pair]
                 pairs[y].append(cur_pair)
+
+                for yy in range(y + 1, min(y + depth + 1, height)):
+                    for i, pair in enumerate(pairs[yy]):
+                        if math.fabs(pair.center - cur_pair.center) < x_eps:
+                            pair.alive = True
+                            cur_track.extend(tracks[yy][i])
+
+                tracks[y].append(cur_track)
+
+        #delete old tracks
+        if y + depth < height:
+            for old_tracks in tracks[y + depth]:
+                old_tracks_len = len(old_tracks)
+                i = 0
+                #while i < old_tracks_len:
+                #    if len(old_tracks[i]) < min_track_len:
+                #        del old_tracks[i]
+                #        old_tracks_len = old_tracks_len - 1
+                #    else:
+                #        i = i + 1
+
 
     for elem in pairs:
         print(len(elem))
@@ -50,7 +79,7 @@ def cross_detect(edge_image):
 
 if __name__ == '__main__':
     # загружаем изображение и отображаем его
-    image = cv2.imread("img0002.jpg", 0)
+    image = cv2.imread("test.png", 0)
 
     image_blur = cv2.GaussianBlur(image, (0, 0), 1)
     cv2.imshow("Gauss blur", image_blur)
